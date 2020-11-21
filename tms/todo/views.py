998695c274b9369f43a3767a6c3609c4a8a272ed
Vishwas. R <vishwasrvibhu@gmail.com>
2020-11-21 22:06:16 +0530

    background images and new bootstrap theme

--- tms/todo/views.py
+++ tms/todo/views.py
@@ -1,15 +1,13 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.db.utils import IntegrityError
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView, View
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 # Create your views here.
 from .models import Task
 from todo.forms import TodoForm
-from django.views import View
-from django.views import generic
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -38,13 +36,31 @@ class Details(DetailView):
 
 class Create(CreateView):
     model = Task
-    fields = ['task','desc']
+    fields = ['task', 'desc']
     success_url = reverse_lazy('todo:items')
 
 
 class Update(UpdateView):
     model = Task
-    fields = ['task', 'desc']
+    template = 'todo/task_update.html'
+    success_url = reverse_lazy('blog:home')
+
+    def get(self, request, pk):
+        task = get_object_or_404(self.model, pk=pk)
+        # print(blog.title)
+        ctx = {'task': task.task, 'desc': task.desc}
+        # print(ctx)
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk):
+        task = get_object_or_404(self.model, pk=pk)
+        form = TodoForm(request.POST, instance=blog)
+        if not form.is_valid():
+            ctx = {'task': task.name, 'desc': task.desc}
+            return render(request, self.template, ctx)
+
+        form.save()
+        return redirect(self.success_url)
     success_url = reverse_lazy('todo:items')
 
 
@@ -54,13 +70,11 @@ class Delete(DeleteView):
     success_url = reverse_lazy('todo:items')
 
 
-from django.views.decorators.csrf import csrf_exempt
-
 @method_decorator(csrf_exempt, name='dispatch')
 class Togglebool(View):
     def post(self, request, pk):
         print("Add PK", pk)
-        t = get_object_or_404(Task,id=pk)
+        t = get_object_or_404(Task, id=pk)
         t.done = not t.done
         try:
             print("hello")
